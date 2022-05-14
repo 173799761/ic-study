4ad8f0dc03301969d007a16aea6041a313e80d36
guoliang <guoliang@gliangliang.com>
2022-05-15 00:28:56 +0800

    init

---
+++ part2/course1/iclogger/src/iclogger_assets/src/index.js
@@ -0,0 +1,19 @@
+import { iclogger } from "../../declarations/iclogger";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await iclogger.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
